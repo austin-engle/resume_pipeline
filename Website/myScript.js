dd5d39fcf5cd80c1c5a78014be39841c77e22051
austin-engle <aengl29@wgu.edu>
2020-09-05 13:47:42 -0600

    got backend javascript function working

--- Website/myScript.js
+++ Website/myScript.js
@@ -1,5 +1,33 @@
-function test() {
-    alert("Test Alert!")
+const sendHttpRequest = (method, url) => {
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
+
+        xhr.open(method, url);
+
+        xhr.onload = () => {
+            resolve(xhr.response);
+        }
+
+        xhr.send();
+    });
+    return promise
+
+};
+
+
+const getData = () => {
+    // https://www.youtube.com/watch?v=4K33w-0-p2c
+    sendHttpRequest('GET', 'https://edp6a47m2d.execute-api.us-west-2.amazonaws.com/development/viewcount')
+        .then(responseData => {
+            console.log(responseData)
+            let count_element = document.getElementById('counter');
+            count_element.innerHTML = responseData
+        })
+
 }
 
-console.log('this is 1')
+// when the webpage loads run the counter function
+window.onload = getData
+
+
